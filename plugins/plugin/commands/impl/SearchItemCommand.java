c465a15a60488514d1f4e4de7cb58a9a94c64bd1
Seven <freesunfury@gmail.com>
2016-08-04 17:06:58 -0500

    Added a command plugin to search items

---
+++ plugins/plugin/commands/impl/SearchItemCommand.java
@@ -0,0 +1,39 @@
+package plugin.commands.impl;
+
+import java.util.Arrays;
+
+import astraeus.game.event.SubscribesTo;
+import astraeus.game.event.impl.CommandEvent;
+import astraeus.game.model.entity.item.ItemDefinition;
+import astraeus.game.model.entity.mob.player.Player;
+import astraeus.game.model.entity.mob.player.PlayerRights;
+import astraeus.net.packet.out.ServerMessagePacket;
+import plugin.commands.Command;
+import plugin.commands.CommandParser;
+
+@SubscribesTo(CommandEvent.class)
+public class SearchItemCommand extends Command {
+
+  @Override
+  protected boolean execute(Player player, CommandParser parser) {
+    if (parser.hasNext(1)) {
+      String keyword = parser.nextString().toLowerCase();
+      
+      Arrays.stream(ItemDefinition.definitions).filter(it -> it.getName().toLowerCase().contains(keyword)).forEach(it -> player.queuePacket(new ServerMessagePacket(it.getId() + ":" + it.getName())));
+      
+      return true;
+    }
+    return false;
+  }
+
+  @Override
+  public boolean test(CommandEvent event) {
+    return event.getName().equalsIgnoreCase("sitem");
+  }
+
+  @Override
+  protected PlayerRights rights() {
+    return PlayerRights.DEVELOPER;
+  }
+
+}

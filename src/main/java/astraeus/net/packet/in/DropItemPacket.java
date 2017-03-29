bbc8f6f9a2c82c4f65836ad26da99cf65949e684
Chad <nshusa99@gmail.com>
2017-03-28 21:29:59 -0500

    Fixed picking up items from duplicating

--- src/main/java/astraeus/net/packet/in/DropItemPacket.java
+++ src/main/java/astraeus/net/packet/in/DropItemPacket.java
@@ -1,6 +1,8 @@
 package astraeus.net.packet.in;
 
 import astraeus.game.model.entity.item.Item;
+import astraeus.game.model.entity.item.ItemContainer;
+import astraeus.game.model.entity.item.ItemContainerPolicy;
 import astraeus.game.model.entity.mob.player.Player;
 import astraeus.game.model.entity.mob.player.PlayerRights;
 import astraeus.game.model.entity.object.GameObjects;
@@ -10,51 +12,67 @@ import astraeus.net.packet.IncomingPacket;
 import astraeus.net.packet.IncomingPacket.IncomingPacketOpcode;
 import astraeus.net.packet.Receivable;
 import astraeus.net.packet.out.AddGroundItemPacket;
+import astraeus.net.packet.out.RemoveGroundItemPacket;
 import astraeus.net.packet.out.ServerMessagePacket;
 
 /**
  * The {@link IncomingPacket} responsible for dropping items.
  * 
- * @author Vult-R
+ * @author Freyr
  */
 @IncomingPacketOpcode(IncomingPacket.DROP_ITEM)
 public final class DropItemPacket implements Receivable {
 
-  @Override
-  public void handlePacket(Player player, IncomingPacket packet) {
-    ByteBufReader reader = packet.getReader();
+	@Override
+	public void handlePacket(Player player, IncomingPacket packet) {
+		ByteBufReader reader = packet.getReader();
 
-    final int itemId = reader.readShort(false, ByteModification.ADDITION);
+		final int itemId = reader.readShort(false, ByteModification.ADDITION);
 
-    reader.readByte(false);
-    reader.readByte(false);
+		reader.readByte(false);
+		reader.readByte(false);
 
-    final int slot = reader.readShort(false, ByteModification.ADDITION);
+		final int slot = reader.readShort(false, ByteModification.ADDITION);
 
-    final Item item = player.getInventory().get(slot);
+		final Item item = player.getInventory().get(slot).copy();
 
-    if (item == null) {
-      return;
-    }
+		if (item == null) {
+			return;
+		}
 
-    // TODO add destoryable items
-    boolean droppable = true;
+		// TODO add destoryable items
+		boolean droppable = true;
 
-    if (!droppable) {
-      player.queuePacket(new ServerMessagePacket("This item cannot be dropped."));
-      return;
-    }
+		if (!droppable) {
+			player.queuePacket(new ServerMessagePacket("This item cannot be dropped."));
+			return;
+		}
 
-    if (player.getRights().equals(PlayerRights.DEVELOPER) && player.attr().get(Player.DEBUG_KEY)) {
-      player.queuePacket(new ServerMessagePacket("ItemDropped: " + itemId));
-    }
+		if (player.getRights().equals(PlayerRights.DEVELOPER) && player.attr().get(Player.DEBUG_KEY)) {
+			player.queuePacket(new ServerMessagePacket("ItemDropped: " + itemId));
+		}
 
-    player.queuePacket(new AddGroundItemPacket(player.getPosition().copy(), item));
-    
-    GameObjects.getGroundItems().put(player.getPosition().copy(), new Item[]{item});
+		player.getInventory().remove(item);
 
-    player.getInventory().remove(item);
+		Item[] items = GameObjects.getGroundItems().get(player.getPosition());
 
-  }
+		ItemContainer container = new ItemContainer(64, ItemContainerPolicy.NORMAL);
+
+		if (items == null) {
+			container.add(item);
+		} else {
+			container.addAll(items);
+			container.add(item);
+		}
+
+		GameObjects.getGroundItems().put(player.getPosition().copy(), container.container());
+		
+		if (item.isStackable()) {
+			player.queuePacket(new RemoveGroundItemPacket(item));
+		}
+
+		player.queuePacket(new AddGroundItemPacket(player.getPosition().copy(), item));
+
+	}
 
 }
--- src/main/java/astraeus/net/packet/in/PickupGroundItemPacket.java
+++ src/main/java/astraeus/net/packet/in/PickupGroundItemPacket.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import astraeus.game.model.Position;
 import astraeus.game.model.entity.item.Item;
+import astraeus.game.model.entity.item.ItemContainer;
+import astraeus.game.model.entity.item.ItemContainerPolicy;
 import astraeus.game.model.entity.mob.player.Player;
 import astraeus.game.model.entity.mob.player.PlayerRights;
 import astraeus.game.model.entity.object.GameObjects;
@@ -18,61 +20,71 @@ import astraeus.net.packet.out.ServerMessagePacket;
 /**
  * The {@link IncomingPacket} responsible for picking up an item on the ground.
  * 
- * @author SeVen
+ * @author Freyr
  */
 @IncomingPacket.IncomingPacketOpcode(IncomingPacket.PICKUP_GROUND_ITEM)
 public final class PickupGroundItemPacket implements Receivable {
 
-  @Override
-  public void handlePacket(final Player player, IncomingPacket packet) {
-    ByteBufReader reader = packet.getReader();
+	@Override
+	public void handlePacket(final Player player, IncomingPacket packet) {
+		ByteBufReader reader = packet.getReader();
 
-    final int y = reader.readShort(ByteOrder.LITTLE);
-    final int id = reader.readShort(false);
-    final int x = reader.readShort(ByteOrder.LITTLE);
+		final int y = reader.readShort(ByteOrder.LITTLE);
+		final int id = reader.readShort(false);
+		final int x = reader.readShort(ByteOrder.LITTLE);
 
-    // create the position object
-    Position position = new Position(x, y, player.getPosition().getHeight());
+		// create the position object
+		Position position = new Position(x, y, player.getPosition().getHeight());
 
-    // get the item from the map
-    Item[] items = GameObjects.getGroundItems().get(position);
+		// get the item from the map
+		Item[] stack = GameObjects.getGroundItems().get(position);
 
-    // validate it exists
-    if (items == null) {
-      return;
-    }
-    
-    Optional<Item> toPickup = Optional.empty();
-    
-    for(Item item : items) {
-      if (item == null) {
-        continue;
-      }
-      
-      if (item.getId() == id) {
-        toPickup = Optional.of(item);
-      }
-    }
-    
-    toPickup.ifPresent(it -> {      
-      
-      if (player.getRights().equals(PlayerRights.DEVELOPER) && player.attr().get(Player.DEBUG_KEY)) {
-        player.queuePacket(new ServerMessagePacket(String
-            .format("[PickupItem] - Item: %s Position: %s", it.toString(), position.toString())));
-      }
+		if (stack == null) {
+			return;
+		}
 
-      player.startAction(new DistancedTask(player, position, 2) {
+		ItemContainer container = new ItemContainer(64, ItemContainerPolicy.NORMAL);
+		container.addAll(stack);
+		
+		if (!container.contains(id)) {
+			return;
+		}
 
-        @Override
-        public void onReached() {
-          player.getInventory().add(it);
-          player.queuePacket(new RemoveGroundItemPacket(it));
-          GameObjects.getGlobalObjects().remove(it);
-        }
+		player.startAction(new DistancedTask(player, position, 2) {
 
-      });      
-      
-    });
+			@Override
+			public void onReached() {
+				
+				if (container.contains(id)) {
+					Optional<Item> result = container.searchItem(id);					
+					
+					if (!result.isPresent()) {
+						return;
+					}
+					
+					Item item = result.get();
 
-  }
+					player.getInventory().add(item);
+					
+					container.remove(item);
+					
+					player.queuePacket(new RemoveGroundItemPacket(item));
+					
+					if (container.size() == 0) {
+						GameObjects.getGroundItems().remove(position);
+					} else {
+						GameObjects.getGroundItems().put(position, container.container());
+					}
+
+					if (player.getRights().equals(PlayerRights.DEVELOPER) && player.attr().get(Player.DEBUG_KEY)) {
+						player.queuePacket(new ServerMessagePacket(String.format("[PickupItem] - Item: %s Position: %s",
+								result.toString(), position.toString())));
+					}
+				}
+				
+			}
+
+		});
+
+	}
 }
